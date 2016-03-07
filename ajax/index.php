f0699a8f8e7289f48c3eed5c70abd5065a579802
Jami <jamischwarzwalder@gmail.com>
2016-03-07 00:11:54 -0800

    Fixing an Ajax error

--- ajax/index.php
+++ ajax/index.php
@@ -1,29 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Ajax Dictionary</title>
+	<style>
+	h3:first-letter
+	{
+		text-transform: uppercase;
+	}
+	</style>
+</head>
 <body>
-
 <h2>Select a term:</h2>
-  <select id='choice'>
+<select id='choice'>
   <option value='ajax'>Ajax</option>
   <option value='button'>Button</option>
-  <option value='load'>Load</option>
-  <option value='texas'>Texas</option>
-  <option value='zenzizenzizenzic'>Zenzizenzizenzic</option>
   <option value='harry'>Harry</option>
-  <option value='zaftig'>Zaftig</option>
+  <option value='load'>Load</option>
   <option value='sesquipedalian'>Sesquipedalian</option>
+  <option value='texas'>Texas</option>
+  <option value='zaftig'>Zaftig</option>
+  <option value='zenzizenzizenzic'>Zenzizenzizenzic</option>
 </select>
   
-<button>Define</button><br /><br />
+<button id="define">Define</button><br /><br />
 <h3><div id="word"></div></h3>
-</body>
-</html>
-
 <p id="definition"></p>
 
 <script src="https://code.jquery.com/jquery-1.12.1.min.js"></script>
 <script>
 
-  $(button).click(function(){
-    var word = $('select').val();
+  $('#define').click(function(){
+    var word = $('#choice').val();
     $.post('ajax-script.php', {'word':word}, function(result){
 		$('#word').html(word);
 	    $('#definition').html(result);
@@ -32,3 +40,5 @@
 
   });
 </script>
+</body>
+</html>
